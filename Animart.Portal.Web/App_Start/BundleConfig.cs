509e1ff72a5b7bf656eb155365f13e732cc4f51f
luckilystar <oscar.wongso@gmail.com>
2016-03-14 17:12:29 +0700

    BundleConfig Changes?

--- Animart.Portal.Web/App_Start/BundleConfig.cs
+++ Animart.Portal.Web/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace Animart.Portal.Web
             bundles.IgnoreList.Clear();
 
             //VENDOR RESOURCES
-
+            BundleTable.EnableOptimizations = false;
             //~/Bundles/App/vendor/css
             bundles.Add(
                 new StyleBundle("~/Bundles/App/vendor/css")
@@ -82,6 +82,8 @@ namespace Animart.Portal.Web
                 new ScriptBundle("~/Bundles/App/Main/js")
                     .IncludeDirectory("~/App/Main", "*.js", true)
                 );
+
+            
         }
     }
 }
